a6cecad7983952d45a0f10ce4a1207633d0cfcfc
geronimovenzi <geronimovenzi@live.com.ar>
2022-09-05 17:18:42 -0300

    add homepage

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
+import { HomePage } from "../components/HomePage/HomePage";
 import { Layout } from "../components/Layout";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <Layout>
-      <h1>Estamos en el home</h1>
+      <HomePage />
     </Layout>
   );
 }
