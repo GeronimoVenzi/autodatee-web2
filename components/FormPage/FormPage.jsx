2959e421d419d63ddb89e548c5ab3d765bf8ae3b
Jeremias Storm <jerestorm@gmail.com>
2022-09-09 00:28:49 -0300

    custom details

--- components/FormPage/FormPage.jsx
+++ components/FormPage/FormPage.jsx
@@ -4,62 +4,65 @@ import Form from "react-bootstrap/Form";
 import styles from "./FormPage.module.css";
 
 export const FormPage = () => {
-    return (
-        <section className="mb-5">
-            <div className="text-center">
-                <h1 className={styles.form__title}>Contactanos</h1>
-            </div>
+  return (
+    <section className="mb-5" id="contacto">
+      <div className="text-center">
+        <h1 className={styles.form__title}>Contactanos</h1>
+      </div>
 
-            <Form className={`${styles.bg_form} container mb-5 mt-5`}>
-                <div className={styles.form__group}>
-                    <div>
-                        <Form.Group
-                            className={`${styles.form__group__input} mb-5`}
-                            controlId="formBasicName"
-                        >
-                            <Form.Control type="name" placeholder="Nombre" />
-                        </Form.Group>
+      <Form className={`${styles.bg_form} container mb-5 mt-5`}>
+        <div className={styles.form__group}>
+          <div>
+            <Form.Group
+              className={`${styles.form__group__input} mb-5`}
+              controlId="formBasicName"
+            >
+              <Form.Control type="name" placeholder="Nombre" />
+            </Form.Group>
 
-                        <Form.Group
-                            className={`${styles.form__group__input} mb-5`}
-                            controlId="formBasicPhone"
-                        >
-                            <Form.Control type="number" placeholder="Telefono" />
-                        </Form.Group>
-                    </div>
-                    <div>
-                        <Form.Group
-                            className={`${styles.form__group__input} mb-5`}
-                            controlId="formBasicEmail"
-                        >
-                            <Form.Control type="email" placeholder="Email" />
-                        </Form.Group>
+            <Form.Group
+              className={`${styles.form__group__input} mb-5`}
+              controlId="formBasicPhone"
+            >
+              <Form.Control type="number" placeholder="Telefono" />
+            </Form.Group>
+          </div>
+          <div>
+            <Form.Group
+              className={`${styles.form__group__input} mb-5`}
+              controlId="formBasicEmail"
+            >
+              <Form.Control type="email" placeholder="Email" />
+            </Form.Group>
 
-                        <Form.Group
-                            className={`${styles.form__group__input} mb-5`}
-                            controlId="formBasicRole"
-                        >
-                            <Form.Control type="text" placeholder="Selecciona un rol" />
-                        </Form.Group>
-                    </div>
-                </div>
+            <Form.Group
+              className={`${styles.form__group__input} mb-5`}
+              controlId="formBasicRole"
+            >
+              <Form.Control type="text" placeholder="Selecciona un rol" />
+            </Form.Group>
+          </div>
+        </div>
 
-                <div className={styles.form__textarea}>
-                    <FloatingLabel
-                        className="mb-5"
-                        controlId="floatingTextarea2"
-                        label="Contanos de tu proyecto"
-                    >
-                        <Form.Control as="textarea" style={{ height: "250px" }} />
-                    </FloatingLabel>
-                </div>
+        <div className={styles.form__textarea}>
+          <FloatingLabel
+            className="mb-5"
+            controlId="floatingTextarea2"
+            label="Contanos de tu proyecto"
+          >
+            <Form.Control as="textarea" style={{ height: "250px" }} />
+          </FloatingLabel>
+        </div>
 
-                <div className="d-flex justify-content-center">
-                    <div className={`${styles.form__btn} btn btn-outline-light`} type="submit">
-                        Enviar
-                    </div>
-                </div>
-            </Form>
-        </section>
-    );
+        <div className="d-flex justify-content-center">
+          <div
+            className={`${styles.form__btn} btn btn-outline-light`}
+            type="submit"
+          >
+            Enviar
+          </div>
+        </div>
+      </Form>
+    </section>
+  );
 };
--- components/PartnersPage/Slider.jsx
+++ components/PartnersPage/Slider.jsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import Image from "next/image";
 import { Autoplay } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
@@ -16,164 +16,132 @@ import Nation from "/components/assets/logos-clientes/nation-sa.png";
 import Yacopini from "/components/assets/logos-clientes/yacopini.png";
 
 export const Slider = () => {
-    // const marcas = [
-    //     {
-    //         nombre: "Prana",
-    //         url: "prana.jpg",
-    //     },
-    //     {
-    //         nombre: "Ruta3",
-    //         url: "descarga.png",
-    //     },
-    //     {
-    //         nombre: "Nation",
-    //         url: "nation-sa.png",
-    //     },
-    //     {
-    //         nombre: "AutoHaus",
-    //         url: "AUTOHAUS.jpg",
-    //     },
-    //     {
-    //         nombre: "FordGoldstein",
-    //         url: "GOLDSTEIN.jpg",
-    //     },
-    //     {
-    //         nombre: "Yacopini",
-    //         url: "yacopini.png",
-    //     },
-    //     {
-    //         nombre: "Giama",
-    //         url: "GIAMA.png",
-    //     },
-    //     {
-    //         nombre: "LeParc",
-    //         url: "le-parc.png",
-    //     },
-    //     {
-    //         nombre: "GrupoAntun",
-    //         url: "grupo-antun.jpg",
-    //     },
-    //     {
-    //         nombre: "AutoCity",
-    //         url: "Autocity-01.png",
-    //     },
-    // ];
+  // const marcas = [
+  //     {
+  //         nombre: "Prana",
+  //         url: "prana.jpg",
+  //     },
+  //     {
+  //         nombre: "Ruta3",
+  //         url: "descarga.png",
+  //     },
+  //     {
+  //         nombre: "Nation",
+  //         url: "nation-sa.png",
+  //     },
+  //     {
+  //         nombre: "AutoHaus",
+  //         url: "AUTOHAUS.jpg",
+  //     },
+  //     {
+  //         nombre: "FordGoldstein",
+  //         url: "GOLDSTEIN.jpg",
+  //     },
+  //     {
+  //         nombre: "Yacopini",
+  //         url: "yacopini.png",
+  //     },
+  //     {
+  //         nombre: "Giama",
+  //         url: "GIAMA.png",
+  //     },
+  //     {
+  //         nombre: "LeParc",
+  //         url: "le-parc.png",
+  //     },
+  //     {
+  //         nombre: "GrupoAntun",
+  //         url: "grupo-antun.jpg",
+  //     },
+  //     {
+  //         nombre: "AutoCity",
+  //         url: "Autocity-01.png",
+  //     },
+  // ];
 
-    return (
-        <div className="container">
-            <div className={styles.swiperContainer}>
-                <Swiper
-                    className="mt-5"
-                    modules={[Autoplay]}
-                    autoplay={{
-                        delay: 800,
-                        disableOnInteraction: false,
-                    }}
-                    slidesPerView={4}
-                    breakpoints={{
-                        "@0.00": {
-                            slidesPerView: 1,
-                            spaceBetween: 25,
-                        },
-                        "@1.00": {
-                            slidesPerView: 2,
-                            spaceBetween: 25,
-                        },
-                        "@1.50": {
-                            slidesPerView: 3,
-                            spaceBetween: 30,
-                        },
-                        "@1.75": {
-                            slidesPerView: 4,
-                            spaceBetween: 20,
-                        },
-                    }}
-                >
-                    <SwiperSlide className={`${styles.swiper__container} mt-5`}>
-                        <div className={styles.card__marca}>
-                            <Image
-                                src={Prana}
-                                alt="Prana"
-                            />
-                        </div>
-                    </SwiperSlide>,
-
-                    <SwiperSlide className={`${styles.swiper__container} mt-5`}>
-                        <div className={styles.card__marca}>
-                            <Image
-                                src={Autocity}
-                                alt="Autocity"
-                            />
-                        </div>
-                    </SwiperSlide>,
-                    <SwiperSlide className={`${styles.swiper__container} mt-5`}>
-                        <div className={styles.card__marca}>
-                            <Image
-                                src={Autohaus}
-                                alt="Autohaus"
-                            />
-                        </div>
-                    </SwiperSlide>,
-                    <SwiperSlide className={`${styles.swiper__container} mt-5`}>
-                        <div className={styles.card__marca}>
-                            <Image
-                                src={Giama}
-                                alt="Giama"
-                            />
-                        </div>
-                    </SwiperSlide>,
-                    <SwiperSlide className={`${styles.swiper__container} mt-5`}>
-                        <div className={styles.card__marca}>
-                            <Image
-                                src={Goldstein}
-                                alt="Goldstein"
-                            />
-                        </div>
-                    </SwiperSlide>,
-                    <SwiperSlide className={`${styles.swiper__container} mt-5`}>
-                        <div className={styles.card__marca}>
-                            <Image
-                                src={Descarga}
-                                alt="Descarga"
-                            />
-                        </div>
-                    </SwiperSlide>,
-                    <SwiperSlide className={`${styles.swiper__container} mt-5`}>
-                        <div className={styles.card__marca}>
-                            <Image
-                                src={Antun}
-                                alt="Antun"
-                            />
-                        </div>
-                    </SwiperSlide>,
-                    <SwiperSlide className={`${styles.swiper__container} mt-5`}>
-                        <div className={styles.card__marca}>
-                            <Image
-                                src={Leparc}
-                                alt="LeParc"
-                            />
-                        </div>
-                    </SwiperSlide>,
-                    <SwiperSlide className={`${styles.swiper__container} mt-5`}>
-                        <div className={styles.card__marca}>
-                            <Image
-                                src={Yacopini}
-                                alt="Yacopini"
-                            />
-                        </div>
-                    </SwiperSlide>,
-                    <SwiperSlide className={`${styles.swiper__container} mt-5`}>
-                        <div className={styles.card__marca}>
-                            <Image
-                                src={Nation}
-                                alt="Nation"
-                            />
-                        </div>
-                    </SwiperSlide>
-
-                </Swiper>
-
+  return (
+    <div className="container">
+      <div className={styles.swiperContainer}>
+        <Swiper
+          className="mt-5"
+          modules={[Autoplay]}
+          autoplay={{
+            delay: 800,
+            disableOnInteraction: false,
+          }}
+          slidesPerView={4}
+          breakpoints={{
+            "@0.00": {
+              slidesPerView: 1,
+              spaceBetween: 25,
+            },
+            "@1.00": {
+              slidesPerView: 2,
+              spaceBetween: 25,
+            },
+            "@1.50": {
+              slidesPerView: 3,
+              spaceBetween: 30,
+            },
+            "@1.75": {
+              slidesPerView: 4,
+              spaceBetween: 20,
+            },
+          }}
+        >
+          <SwiperSlide className={`${styles.swiper__container} mt-5`}>
+            <div className={styles.card__marca}>
+              <Image src={Prana} alt="Prana" />
             </div>
-        </div>
-    );
+          </SwiperSlide>
+
+          <SwiperSlide className={`${styles.swiper__container} mt-5`}>
+            <div className={styles.card__marca}>
+              <Image src={Autocity} alt="Autocity" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className={`${styles.swiper__container} mt-5`}>
+            <div className={styles.card__marca}>
+              <Image src={Autohaus} alt="Autohaus" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className={`${styles.swiper__container} mt-5`}>
+            <div className={styles.card__marca}>
+              <Image src={Giama} alt="Giama" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className={`${styles.swiper__container} mt-5`}>
+            <div className={styles.card__marca}>
+              <Image src={Goldstein} alt="Goldstein" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className={`${styles.swiper__container} mt-5`}>
+            <div className={styles.card__marca}>
+              <Image src={Descarga} alt="Descarga" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className={`${styles.swiper__container} mt-5`}>
+            <div className={styles.card__marca}>
+              <Image src={Antun} alt="Antun" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className={`${styles.swiper__container} mt-5`}>
+            <div className={styles.card__marca}>
+              <Image src={Leparc} alt="LeParc" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className={`${styles.swiper__container} mt-5`}>
+            <div className={styles.card__marca}>
+              <Image src={Yacopini} alt="Yacopini" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className={`${styles.swiper__container} mt-5`}>
+            <div className={styles.card__marca}>
+              <Image src={Nation} alt="Nation" />
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
+  );
 };
