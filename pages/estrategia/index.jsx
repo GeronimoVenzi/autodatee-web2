632dc6868713c09c1eee6985284f4136baeafae4
Jeremias Storm <jerestorm@gmail.com>
2022-09-11 21:44:59 -0300

    Starting Estrategia Page

--- pages/estrategia/index.jsx
+++ pages/estrategia/index.jsx
@@ -1,7 +1,17 @@
 import { Layout } from "../../components/Layout";
+import styles from "../../styles/Estrategia.module.css";
 
 const Estrategia = () => {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <section className={`section ${styles.bg__estrategia}`}>
+        <div className={`title__principal font__mont`}>
+          <h1>ESTRATEGIA</h1>
+          <p>Pienso, luego existo</p>
+        </div>
+      </section>
+    </Layout>
+  );
 };
 
 export default Estrategia;
