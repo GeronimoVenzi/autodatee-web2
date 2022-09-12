228c62c27e823c811cbd37223c85637566bbf315
geronimovenzi <geronimovenzi@live.com.ar>
2022-09-11 23:39:09 -0300

    add and custom estrategia

---
+++ components/section-cards/Cards.jsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+
+import { Card } from "react-bootstrap";
+
+export const Cards = ({ children, data }) => {
+
+    return (
+        <>
+            <Card className='card__section'>
+                <Card.Header className='card__header'>
+                    <Image src={children} alt="icono" />
+                    <Card.Title className='text-center'> {data.title} </Card.Title>
+                </Card.Header>
+                <Card.Body>
+                    <Card.Text className='text-center'> {data.text} </Card.Text>
+                </Card.Body>
+
+
+            </Card>
+        </>
+    )
+}
--- pages/estrategia/index.jsx
+++ pages/estrategia/index.jsx
@@ -1,13 +1,53 @@
 import { Layout } from "../../components/Layout";
+import { Cards } from "../../components/section-cards/Cards";
 import styles from "../../styles/Estrategia.module.css";
 
+import Modelo from '../../components/assets/secciones/Estrategia/icono1.png';
+import Consultoria from '../../components/assets/secciones/Estrategia/icono2.png';
+
 const Estrategia = () => {
+  const card1 = {
+    title: 'Modelo de negocios',
+    text: 'Atracción de clientes con contenido útil y relevante. Ayudamos a definir con claridad la oferta de la empresa al mercado. Qué, quiénes, cómo y de qué forma se va a posicionar. Estructuramos las partes que componen la emprea en torno a la propuesta de valor para diferenciarse de la competencia y fidelizar a los clientes.'
+  }
+
+  const card2 = {
+    title: 'Consultoría',
+    text: 'Auxiliamos a los clientes a identificar mercados clave, consumidores de interés y estrategias de marketing. Mantenemos una comunicación constante y reuniones estratégicas para analizar los resultados.'
+  }
   return (
     <Layout>
       <section className={`section ${styles.bg__estrategia}`}>
         <div className={`title__principal font__mont`}>
           <h1>ESTRATEGIA</h1>
           <p>Pienso, luego existo</p>
+        </div>
+      </section>
+      <section className={`${styles.section__estrategia} section`}>
+        <div className="cards__container">
+          <Cards data={card1}>
+            {Modelo}
+          </Cards>
+          <Cards data={card2}>
+            {Consultoria}
+          </Cards>
+        </div>
+        <div className={`buttons__container`}>
+          <div className={styles.btn__estrategia}>DIAGNÓSTICO</div>
+          <div className={styles.btn__estrategia}>SEGMENTACIÓN</div>
+          <div className={styles.btn__estrategia}>TARGETIZACIÓN</div>
+          <div className={styles.btn__estrategia}>POSICIONAMIENTO</div>
+        </div>
+        <div className="text__cards">
+          <p>
+            Trabajamos <b>en conjunto con nuestro cliente</b> para lograr la idea, los pasos
+            y los  <br /> objetivos que cumpliremos con la implementación de esta objetiva y
+            creativa idea.
+          </p>
+          <p>
+            Aplicamos <b>matrices de marketing y metodologías</b> para lograr <br /> ideas innovadoras
+            y diferenciandolas en el mercado.
+          </p>
         </div>
       </section>
     </Layout>
