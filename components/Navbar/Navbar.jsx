1b8b0b048cf63eaf3545b1f33f347eb1e372ebe5
geronimovenzi <geronimovenzi@live.com.ar>
2022-09-13 13:48:22 -0300

    add dropwdown menu in navbar

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import Offcanvas from "react-bootstrap/Offcanvas";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { NavDropdown } from "react-bootstrap";
 
 import Logo from "/components/assets/Logos/AD-logo.png";
 import styles from "./Navbar.module.css";
@@ -44,7 +45,17 @@ export const NavBar = () => {
               >
                 <Nav.Link href="#inicio">Home</Nav.Link>
                 <Nav.Link href="#nosotros">Sobre nosotros</Nav.Link>
-                <Nav.Link href="#servicios">Servicios</Nav.Link>
+                <NavDropdown
+                  title="Servicios"
+                  id={`offcanvasNavbarDropdown-expand`}
+                >
+                  <NavDropdown.Item href="/estrategia">Estrategia</NavDropdown.Item>
+                  <NavDropdown.Item href="/creatividad">Creatividad y Contenidos</NavDropdown.Item>
+                  <NavDropdown.Item href="/relaciones">Relaciones Públicas</NavDropdown.Item>
+                  <NavDropdown.Item href="/social">Social Media</NavDropdown.Item>
+                  <NavDropdown.Item href="/gestion">Gestión de Crisis</NavDropdown.Item>
+                  <NavDropdown.Item href="/leads">Generación de Leads</NavDropdown.Item>
+                </NavDropdown>
                 <Nav.Link href="#contacto">Contactanos</Nav.Link>
               </Nav>
             </Offcanvas.Body>
