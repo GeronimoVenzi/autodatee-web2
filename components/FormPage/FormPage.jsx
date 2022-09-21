c725cf6e7b51c4905cc1d9fddc155364a7dff0ee
geronimovenzi <geronimovenzi@live.com.ar>
2022-09-21 19:58:44 -0300

    add mail in formpage

--- components/FormPage/FormPage.jsx
+++ components/FormPage/FormPage.jsx
@@ -10,21 +10,26 @@ export const FormPage = () => {
         <h1 className={styles.form__title}>Contactanos</h1>
       </div>
 
-      <Form className={`${styles.bg_form} container mb-5 mt-5 font__mont`}>
+      <Form
+        action="https://formsubmit.co/info@autodatee.com.ar"
+        method="POST"
+        className={`${styles.bg_form} container mb-5 mt-5 font__mont`}>
         <div className={styles.form__group}>
           <div>
             <Form.Group
               className={`${styles.form__group__input} mb-5`}
               controlId="formBasicName"
             >
-              <Form.Control type="name" placeholder="Nombre" />
+              <Form.Control
+
+                type="text" name="Nombre" placeholder="Nombre" required />
             </Form.Group>
 
             <Form.Group
               className={`${styles.form__group__input} mb-5`}
               controlId="formBasicPhone"
             >
-              <Form.Control type="number" placeholder="Telefono" />
+              <Form.Control type="number" name="Telefono" placeholder="Telefono" required />
             </Form.Group>
           </div>
           <div>
@@ -32,35 +37,36 @@ export const FormPage = () => {
               className={`${styles.form__group__input} mb-5`}
               controlId="formBasicEmail"
             >
-              <Form.Control type="email" placeholder="E-mail" />
+              <Form.Control type="email" name="E-mail" placeholder="E-mail" required />
             </Form.Group>
 
             <Form.Group
               className={`${styles.form__group__input} mb-5`}
               controlId="formBasicRole"
             >
-              <Form.Control type="text" placeholder="Selecciona un rol" />
+              <Form.Control type="text" name="Rol" placeholder="Selecciona un rol" required />
             </Form.Group>
           </div>
         </div>
 
         <div className={`${styles.form__textarea}`}>
           <FloatingLabel
+
             className="mb-5"
             controlId="floatingTextarea2"
             label="Contanos de tu proyecto"
           >
-            <Form.Control as="textarea" style={{ height: "250px" }} />
+            <Form.Control name="Proyecto" as="textarea" style={{ height: "250px" }} />
           </FloatingLabel>
         </div>
 
         <div className="d-flex justify-content-center">
-          <div
+          <button
             className={`${styles.form__btn} btn btn-outline-light`}
             type="submit"
           >
             Enviar
-          </div>
+          </button>
         </div>
       </Form>
     </section>
